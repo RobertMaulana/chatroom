b69a5c6936d9faec73d1c60f155f4551c9575771
zidane9 <d4rr@hotmail.com>
2017-04-13 10:32:09 +0700

    passport n jwt

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const router = express.Router();
 let controller  = require('../controllers/chat_controller');
+let helpers = require('../helpers/token');
 
 /* GET home page. */
-router.get('/',  controller.getAll);
-router.get('/:id', controller.getOne);
-router.post('/', controller.createOne);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.deleteOne);
+router.get('/', helpers.authenticate,  controller.getAll);
+router.get('/:id', helpers.authenticate, controller.getOne);
+router.post('/', helpers.authenticate, controller.createOne);
+router.put('/:id', helpers.authenticate, controller.update);
+router.delete('/:id', helpers.authenticate, controller.deleteOne);
 
 module.exports = router;
